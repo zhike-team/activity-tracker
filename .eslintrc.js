9fb3002470883865244f4203767c2ae5938732a0
wansong <betterofsong@gmail.com>
2018-07-20 13:21:14 +0800

    Init: implemented

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+      "node": true,
+      "es6": true,
+      "browser": true
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "extends": "eslint:recommended"
+};
---
+++ main.js
@@ -0,0 +1,66 @@
+export default function createActivityTracker ({ onBecomeInactive, INACTIVE_WAIT_TIME = 300000, ctx }) {
+  const instance = {
+    options: { onBecomeInactive, INACTIVE_WAIT_TIME, ctx },
+    sessionStartTime: null,
+    countdownTimer: null
+  }
+
+  const startSession = () => {
+    instance.sessionStartTime = new Date()
+    resumeCountdown()
+  }
+
+  const hasSession = () => {
+    return !!instance.sessionStartTime
+  }
+
+  const resumeCountdown = () => {
+    if (instance.countdownTimer) {
+      clearTimeout(instance.countdownTimer)
+    }
+    instance.countdownTimer = setTimeout(closeSession, instance.options.INACTIVE_WAIT_TIME)
+  }
+
+  const pauseCountdown = () => {
+    if (instance.countdownTimer) {
+      clearTimeout(instance.countdownTimer)
+    }
+  }
+
+  const closeSession = () => {
+    // FIXME: 是否有必要
+    if (instance.countdownTimer) {
+      clearTimeout(instance.countdownTimer)
+    }
+    if (!hasSession()) {
+      return
+    }
+    const sessionStartTime = instance.sessionStartTime
+    instance.sessionStartTime = null
+
+    const sessionEndTime = new Date()
+    const { onBecomeInactive, ctx } = instance.options
+    onBecomeInactive({
+      startTime: sessionStartTime,
+      endTime: sessionEndTime,
+      ctx
+    })
+  }
+
+  const updateActivity = () => {
+    if (!hasSession()) {
+      startSession()
+    } else {
+      resumeCountdown()
+    }
+  }
+
+
+  resumeCountdown()
+
+  return {
+    updateActivity,
+    pauseCountdown,
+    resumeCountdown
+  }
+}
